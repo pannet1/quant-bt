5538aa9b7f8988a9a378e0c4abdda31f8fe6529c
karthick <prog@ecomsense.in>
2023-08-17 06:43:51 +0530

    finplot buy sell arrows

--- quant_bt/simple.py
+++ quant_bt/simple.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from indicators.ta import Average_True_Range
 from time import sleep
-from plots.simple import Plot
+# from plots.simple import Plot
+import finplot as fplt
 
 # Define  Bands parameters
 window = 20
@@ -29,53 +30,77 @@ for symbol in unique_symbols:
     quantity = 1  # Quantity to buy/sell
     buy_price = 0
     sell_price = 0
-    buy_arrow_coords = []
-    sell_arrow_coords = []
 
+    symbol_data['sell_arrow'] = None
+    symbol_data['buy_arrow'] = None
     for i in range(window, len(symbol_data)):
         if ((symbol_data['Close'][i] < symbol_data['top1'][i])
-                and (symbol_data['Open'][i] > symbol_data['Close'][i])):
+                and (symbol_data['Open'][i] > symbol_data['top1'][i])):
             if position is None:
                 position = 'short'
                 sell_price = symbol_data['Close'][i]
                 capital += sell_price * quantity
-                buy_arrow_coords.append(
-                    (symbol_data.index[i], symbol_data['Low'][i]))
+                symbol_data['sell_arrow'][i] = symbol_data['High'][i]
         elif ((symbol_data['Close'][i] > symbol_data['bott1'][i])
-              and (symbol_data['Open'][i] < symbol_data['Close'][i])):
+              and (symbol_data['Open'][i] < symbol_data['bott1'][i])):
             if position is None:
                 position = 'long'
                 buy_price = symbol_data['Close'][i]
                 capital -= buy_price * quantity
-                sell_arrow_coords.append(
-                    (symbol_data.index[i], symbol_data['High'][i]))
+                symbol_data['buy_arrow'][i] = symbol_data['Low'][i]
         # Close positions if necessary
         if position == "short":
             if (
-                (symbol_data['Close'][i] > symbol_data['top2'][i])
-                and (symbol_data['Open'][i] < symbol_data['Close'][i])
+                ((symbol_data['Close'][i] > symbol_data['top2'][i])
+                 and (symbol_data['Open'][i] <= symbol_data['top2'][i]))
+                or (symbol_data['Close'][i] < symbol_data['bott1'][i])
             ):
                 position = None
                 buy_price = symbol_data['Close'][i]
                 capital -= buy_price * quantity
-                buy_arrow_coords.append(
-                    (symbol_data.index[i], symbol_data['Low'][i])
-                )
+                symbol_data['buy_arrow'][i] = symbol_data['Low'][i]
         elif position == "long":
             if (
-                (symbol_data['Close'][i] < symbol_data['bott2'][i])
-                and (symbol_data['Open'][i] > symbol_data['Close'][i])
+                ((symbol_data['Close'][i] < symbol_data['bott2'][i])
+                 and (symbol_data['Open'][i] >= symbol_data['bott2'][i]))
+                or (symbol_data['Close'][i] > symbol_data['top1'][i])
             ):
                 position = None
                 sell_price = symbol_data['Close'][i]
                 capital += sell_price * quantity
-                sell_arrow_coords.append(
-                    (symbol_data.index[i], symbol_data['High'][i]))
+                symbol_data['sell_arrow'][i] = symbol_data['High'][i]
 
     print(f"Final Capital for {symbol}: {capital}")
     symbol_data.to_csv(f"{symbol}.csv")
+    # open symbol.csv and read it in pandas
+    symbol_data = pd.read_csv(f"{symbol}.csv")
+    df = symbol_data.rename(columns={
+                            'Timestamp': 'time', 'Open': 'open', 'Close': 'close', 'High': 'high', 'Low': 'low', 'Volume': 'volume'})
+    df = df.astype({'time': 'datetime64[ns]'})
 
+# create two axes
+    ax = fplt.create_plot(symbol)
+
+# plot candle sticks
+    candles = df[['time', 'open', 'close', 'high', 'low']]
+    fplt.candlestick_ochl(candles, ax=ax)
+    fplt.plot(df['time'], df['top2'], ax=ax, legend='UPPER2')
+    fplt.plot(df['time'], df['top1'], ax=ax, legend='UPPER1')
+    fplt.plot(df['time'], df['sma'], ax=ax, legend='MA')
+    fplt.plot(df['time'], df['bott1'], ax=ax, legend='LOWER1')
+    fplt.plot(df['time'], df['bott2'], ax=ax, legend='LOWER2')
+    fplt.plot(df['time'], df['buy_arrow'], ax=ax,
+              color='#4a5', style='^', width=2, legend='buy')
+    fplt.plot(df['time'], df['sell_arrow'], ax=ax,
+              color='red', style='v', width=2, legend='sell')
+
+    fplt.autoviewrestore()
+
+    # we're done
+    fplt.show()
+    """
     lst = [symbol_data['top2'], symbol_data['top1'], symbol_data['sma'],
            symbol_data['bott1'], symbol_data['bott2']]
     Plot(symbol, symbol_data[['Open', 'Close', 'High', 'Low']], lst)
+    """
     sleep(10)
