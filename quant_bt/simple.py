c8139d2c52b98612900cdb86fe1477e1669cc3d8
karthick <prog@ecomsense.in>
2023-08-21 00:10:46 +0530

    try to plot returns

--- quant_bt/simple.py
+++ quant_bt/simple.py
@@ -1,9 +1,11 @@
-import os
 from indicators.ta import Average_True_Range
 from downloaders.ao_dl import AoDl
+import quantstats as qs
+import os
 from datetime import time
 import finplot as fplt
 import pandas as pd
+from time import sleep
 
 # finplot settings
 fplt.background = "black"
@@ -20,15 +22,16 @@ fibratio3 = 4.236
 
 current_file_path = __file__
 current_file_name_with_extension = os.path.basename(current_file_path)
-startegy, _ = os.path.splitext(current_file_name_with_extension)
+strategy, _ = os.path.splitext(current_file_name_with_extension)
+# get data from angelone
 historicParam = {
     "exchange": "NSE",
     "interval": "FIVE_MINUTE",
     "fromdate": "2023-05-01 09:00",
     "todate": "2023-08-18 09:16"
 }
-aodl = AoDl(startegy, hist_param=historicParam, dir_path=dir_path)
-data = pd.read_csv(f'data/{startegy}/output.csv', parse_dates=['Timestamp'])
+# aodl = AoDl(strategy, hist_param=historicParam, dir_path=dir_path)
+data = pd.read_csv(f'data/{strategy}/output.csv', parse_dates=['Timestamp'])
 unique_symbols = data['Symbol'].unique()
 
 for symbol in unique_symbols:
@@ -53,6 +56,7 @@ for symbol in unique_symbols:
 
     symbol_data['sell_arrow'] = None
     symbol_data['buy_arrow'] = None
+    symbol_data['returns'] = None
     SUP = RES = False
     for i in range(window, len(symbol_data)):
         if (
@@ -71,6 +75,7 @@ for symbol in unique_symbols:
                 sell_price = symbol_data['Close'][i]
                 capital += sell_price * quantity
                 symbol_data['sell_arrow'][i] = symbol_data['High'][i]
+                # symbol_data.loc[i, 'sell_arrow'] = symbol_data['High'][i]
                 RES = False
             elif (
                 SUP and
@@ -80,6 +85,7 @@ for symbol in unique_symbols:
                 buy_price = symbol_data['Close'][i]
                 capital -= buy_price * quantity
                 symbol_data['buy_arrow'][i] = symbol_data['Low'][i]
+                # symbol_data[i, 'buy_arrow'] = symbol_data['Low'][i]
                 SUP = False
         elif position == "short":
             if (
@@ -92,6 +98,8 @@ for symbol in unique_symbols:
                 buy_price = symbol_data['Close'][i]
                 capital -= buy_price * quantity
                 symbol_data['buy_arrow'][i] = symbol_data['Low'][i]
+                # symbol_data[i, 'buy_arrow'] = symbol_data['Low'][i]
+                symbol_data['returns'][i] = sell_price - buy_price
         elif position == "long":
             if (
                 (symbol_data.index[i].time() > exit_before) or
@@ -103,19 +111,36 @@ for symbol in unique_symbols:
                 sell_price = symbol_data['Close'][i]
                 capital += sell_price * quantity
                 symbol_data['sell_arrow'][i] = symbol_data['High'][i]
-
+                # symbol_data.loc[i, 'sell_arrow'] = symbol_data['High'][i]
+                symbol_data['returns'][i] = sell_price - buy_price
     print(f"Final Capital for {symbol}: {capital}")
-    symbol_data.to_csv(f"data/{startegy}/{symbol}.csv")
+    symbol_data.to_csv(f"data/{strategy}/{symbol}.csv")
+    """
+     calculate tearsheet
+    df = symbol_data.dropna(subset=['returns'])
+    df = df[['returns']]
+    df['Date'] = df.index.date
+    df = df.groupby('Date').sum()
+    # Convert the index to a datetime index (if not already)
+    df.index = pd.to_datetime(df.index)
+
+    # Remove time zone information from the index
+    df.index = df.index.tz_localize(None)
+    print(df.tail(5))
+    # extend pandas functionality with metrics, etc.
+    qs.extend_pandas()
+    df.plot_earnings()
+    # qs.reports.metrics(df)
+    sleep(100)
+    """
     # open symbol.csv and read it in pandas
-    symbol_data = pd.read_csv(f"data/{startegy}/{symbol}.csv")
+    symbol_data = pd.read_csv(f"data/{strategy}/{symbol}.csv")
     df = symbol_data.rename(columns={
         'Timestamp': 'time', 'Open': 'open', 'Close': 'close', 'High': 'high', 'Low': 'low', 'Volume': 'volume'})
     df = df.astype({'time': 'datetime64[ns]'})
-
-# create axes
+    # create axes
     ax = fplt.create_plot(symbol)
-
-# plot candle sticks
+    # plot candle sticks
     candles = df[['time', 'open', 'close', 'high', 'low']]
     fplt.candlestick_ochl(candles, ax=ax)
     fplt.plot(df['time'], df['top2'], ax=ax, legend='UPPER2')
@@ -127,13 +152,11 @@ for symbol in unique_symbols:
               color='#4a5', style='^', width=2, legend='buy')
     fplt.plot(df['time'], df['sell_arrow'], ax=ax,
               color='orange', style='v', width=2, legend='sell')
-
     fplt.autoviewrestore()
 
     def save():
         fplt.screenshot(open('screenshots/' + symbol + '.png', 'wb'))
     # wait some until we're rendered
     fplt.timer_callback(save, 0.5, single_shot=True)
-
     # we're done
     fplt.show()
